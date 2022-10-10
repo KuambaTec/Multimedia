73313fec2427f845c319fc813e1afab77ac98532
Pedro Kuamba <infokuamba000@gmail.com>
2022-10-10 18:43:29 -0300

    primeiro comit

---
+++ js/proj.js
@@ -0,0 +1,11 @@
+/*const car = document.querySelector('.car');
+const transito = document.querySelector('.transito');
+
+const laco = setInterval(() => {
+    const carposicao = car.offsetLeft;
+
+    if (carposicao <= 120) {
+        car.style.animation = 'none';
+        car.style.left = `${carposicao}px`;
+    }
+}, 10);*/
